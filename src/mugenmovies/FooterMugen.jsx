04c80bf74c1c4eddeb66060fc65c9801249bff15
Ellisvelandia <eyis619l@gmail.com>
2023-03-09 16:53:02 -0500

    footer

--- src/mugenmovies/FooterMugen.jsx
+++ src/mugenmovies/FooterMugen.jsx
@@ -2,18 +2,20 @@ import React from "react";
 
 const FooterMugen = () => {
   return (
-    <footer
-      className="bg-black relative md:text-right text-center w-full text-white p-8"
-    >
+    <footer className="bg-black relative md:text-right text-center w-full text-white p-8">
       <div className="md:text-base text-xs mb-3 gap-2 inline-flex items-center">
         <a href="http://aniplexusa.com/" target="_blank">
           Aniplex of America
         </a>
         <div className="border-r h-3 "></div>
-     
+
         <a href="https://kimetsu.com/anime/" target="_blank">
           Japanese Official Website
         </a>
+        <div className="border-r h-3 "></div>
+        <div className="inline-block gap-2">
+          <p>©吾峠呼世晴／集英社・アニプレックス・ufotable</p>
+        </div>
       </div>
     </footer>
   );
--- src/pages/Tanjiro.jsx
+++ src/pages/Tanjiro.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import FooterMugen from "../mugenmovies/FooterMugen";
 import Introduction from "../resolve_arc/Introduction";
 import ListMoviesReveal from "../resolve_arc/ListMoviesReveal";
 
@@ -10,19 +12,21 @@ const Tanjiro = () => {
           src="https://res.cloudinary.com/dr49dbp8d/image/upload/v1678388898/demon%20slayer/img-main-pc_huadb9.webp"
           alt="reveal arc"
         />
-        <div className="absolute mt-[8%] top-[55%] w-full">
-          <div className="w-full flex justify-center">
-            <img
-              src="https://res.cloudinary.com/dr49dbp8d/image/upload/v1678389067/demon%20slayer/logo_hzjwef.webp"
-              alt="logo kimetsu"
-              className="w-[35%]"
-            />
+        <Link to="/">
+          <div className="absolute mt-[8%] top-[55%] w-full">
+            <div className="w-full flex justify-center">
+              <img
+                src="https://res.cloudinary.com/dr49dbp8d/image/upload/v1678389067/demon%20slayer/logo_hzjwef.webp"
+                alt="logo kimetsu"
+                className="w-[35%]"
+              />
+            </div>
           </div>
-        </div>
+        </Link>
       </div>
       <ListMoviesReveal />
       <Introduction />
-
+      <FooterMugen />
     </div>
   );
 };
