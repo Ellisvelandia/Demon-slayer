6452b2df553140ea4a41c41ac2dc2bc35290fec2
Ellisvelandia <eyis619l@gmail.com>
2023-03-12 03:42:09 -0500

    purge

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,30 @@
+import htmlPurge from "vite-plugin-purgecss";
+import { createHtmlPlugin } from "vite-plugin-html";
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const root = resolve(__dirname, "./");
+const outDir = resolve(__dirname, "dist");
+
 export default defineConfig({
-  plugins: [react()],
+  root,
+  plugins: [
+    react(),
+    htmlPurge(),
+    createHtmlPlugin({
+      minify: true,
+      removeComments: true,
+      collapseWhitespace: true,
+    }),
+  ],
   build: {
+    outDir,
+    emptyOutDir: true,
     rollupOptions: {
+      input: {
+        main: resolve(root, "index.html"),
+      },
       output: {
         chunkSizeWarningLimit: 1500,
 
